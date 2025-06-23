498ad4cf9ecfbdc5106bf8b6869beec7c61f575a
paraelyolo <paraelyolo@gmail.com>
2025-06-23 16:00:28 -0600

    Add files via upload

--- utils/drive.py
+++ utils/drive.py
@@ -2,13 +2,11 @@ import requests
 import pandas as pd
 from io import StringIO
 
-def descargar_csv_drive(file_id):
+def descargar_csv_drive(file_id, con_encabezado=True):
     """
-    Descarga un CSV desde Google Drive y lo convierte a DataFrame de pandas,
-    forzando que no tenga encabezado.
+    Descarga un CSV desde Google Drive y lo convierte a un DataFrame de pandas.
     """
     URL = "https://docs.google.com/uc?export=download"
-
     session = requests.Session()
 
     response = session.get(URL, params={'id': file_id}, stream=True)
@@ -20,13 +18,15 @@ def descargar_csv_drive(file_id):
 
     content = response.content.decode('utf-8')
     print("Contenido CSV descargado:")
-    print(content[:500])  # primeras 500 letras
+    print(content[:500])  # muestra las primeras 500 letras
 
-    return pd.read_csv(StringIO(content), header=None)  # 👈 importante
+    if con_encabezado:
+        return pd.read_csv(StringIO(content))
+    else:
+        return pd.read_csv(StringIO(content), header=None)
 
 def _get_confirm_token(response):
     for key, value in response.cookies.items():
         if key.startswith('download_warning'):
             return value
     return None
-
